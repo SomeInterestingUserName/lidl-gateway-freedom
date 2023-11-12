d4122b68196e9196a2a920533401230e6c336ff1
SomeInterestingUserName <jchen2120212@gmail.com>
2023-11-12 13:26:58 -0500

    Add extra commands for rootfs checksum

--- scripts/rootfs_tool.py
+++ scripts/rootfs_tool.py
@@ -51,7 +51,8 @@ def _checksum_buffer(b, checksum=0):
     return checksum
 
 def cmd_build(args):
-
+    # if checksum_only is True, only write the checksum value to the output file, nothing else.
+    checksum_only = ('checksum_only' in args and args.checksum_only) 
     squashfs_size = os.path.getsize(args.squashfs_image) \
             - SIZE_OF_SQFS_SUPER_BLOCK 
     if (squashfs_size % 2) != 0:
@@ -69,7 +70,8 @@ def cmd_build(args):
 
         with open(args.rootfs_image, "wb") as fOut:
             checksum = _checksum_buffer(header_prefix)
-            fOut.write(header_prefix)
+            if not checksum_only:
+                fOut.write(header_prefix)
 
             while True:
                 b = fIn.read(1024)
@@ -78,10 +80,52 @@ def cmd_build(args):
                 if (len(b) % 2) != 0:
                     b += b"\0"
                 checksum = _checksum_buffer(b, checksum)
-                fOut.write(b)
+                if not checksum_only:
+                    fOut.write(b)
 
             fOut.write(struct.pack(">H", (0x10000 - checksum)&0xFFFF))
 
+# Calls a modified cmd_build with the appropriate "output" file (contained in checksum_file) and tells it to only write the checksum
+def cmd_sum(args):
+    vars(args)['checksum_only'] = True
+    vars(args)['rootfs_image'] = args.checksum_file
+    cmd_build(args)
+
+def cmd_extract(args):
+    file_size = os.path.getsize(args.rootfs_flash_image)
+    with open(args.rootfs_flash_image, "rb") as f:
+        
+        header_type = ">IIIIIH"
+        with open(args.squashfs_out, "wb") as fOut:
+            b = f.read(struct.calcsize(header_type))
+            (magic, inode_count, modification_time,
+             block_size, fragment_entry_count, compression_id) = struct.unpack(
+                     header_type, b)
+            
+            fOut.write(b)
+
+            if magic != SQUASHFS_MAGIC_BE:
+                raise RuntimeError("Doesn't appear to be a SquashFS.")
+
+            # RTL have repurposed this header to be the size for checksum purposes
+            rtl_size = modification_time + SIZE_OF_SQFS_SUPER_BLOCK + 2
+            if rtl_size > file_size:
+                raise RuntimeError("Not an RTL Squash filesystem")
+
+            f.seek(0) 
+            checksum = 0
+            for _ in range((rtl_size//2)):
+                b = f.read(2)
+                word, = struct.unpack(">H", b)
+                # Write all but the last 2 bytes
+                if f.tell() != file_size:
+                    fOut.write(b)
+                checksum += word
+                checksum &= 0xFFFF
+            if checksum != 0:
+                print("Checksum failed")
+            else:
+                print("Checksum passed")
 
 if __name__=="__main__":
 
@@ -96,6 +140,17 @@ if __name__=="__main__":
     p.add_argument("squashfs_image", type=str)
     p.add_argument("rootfs_image", type=str)
     p.set_defaults(func=cmd_build)
+
+    # Added a convenience function that takes a squashfs file and outputs the checksum to a new file
+    p = subparsers.add_parser("sum", help="Only output the RTL RootFS checksum of a SquashFS")
+    p.add_argument("squashfs_image", type=str)
+    p.add_argument("checksum_file", type=str)
+    p.set_defaults(func=cmd_sum)
+
+    p = subparsers.add_parser("extract", help="Check RTL RootFS image, and extract the SquashFS from the image (without the checksum)")
+    p.add_argument("rootfs_flash_image", type=str)
+    p.add_argument("squashfs_out", type=str)
+    p.set_defaults(func=cmd_extract)
 
     args = parser.parse_args()
     if getattr(args, "func", None):
